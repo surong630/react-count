1cf6772f8bfc7790f24dfa128f4dd8d3d2dbc6ad
swf <947737976@qq.com>
2021-04-19 00:15:49 +0800

    添加404页面

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,64 @@
-import Home from 'components/Home';
 import React from 'react';
-import { BrowserRouter,Router, Route, Link, Switch} from 'react-router-dom'
-import MyHome from './components/Home'
-import MyBox from './components/Box'
+import { BrowserRouter,Router, Route, Link, Switch, Redirect} from 'react-router-dom'
 function App() {
   return (
       <BrowserRouter>
-    <div className="App">
-        <div>
-          <Link to="/home">家</Link>
-          <Link to="/box">盒子</Link>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">
+                Money
+              </Link>
+              </li>
+            <li>
+              <Link to="/tags">
+                Tags
+              </Link>
+            </li>
+            <li>
+              <Link to="/statistics">
+                Statistics
+              </Link>
+
+            </li>
+          </ul>
+        </nav>
           <Switch>
-            <Route path="/home" component={MyHome}></Route>
-            <Route path="/box" component={ MyBox }></Route>
+            <Route path="/home" component={ Money }></Route>
+            <Route path="/tags" component={ Tags }></Route>
+            <Route path="/statistics" component={ Statistics }></Route>
+            <Redirect exact path="/" to="/tags" ></Redirect>
+            <Route path="*" component={Error}></Route>
           </Switch>
-        </div>
-    </div>
       </BrowserRouter>
   );
 }
-
+function Error () {
+  return (
+    <div>
+      你呀输错了知道吗
+    </div>
+  )
+}
+function Money() {
+  return (
+    <div>
+      Money
+    </div>
+  )
+}
+function Tags() {
+  return (
+    <div>
+      Tags
+    </div>
+  )
+}
+function Statistics() {
+  return (
+    <div>
+      Statistics
+    </div>
+  )
+}
 export default App;
